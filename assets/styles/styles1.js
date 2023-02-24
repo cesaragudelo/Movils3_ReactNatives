4ce5f3e0c789ff7035ead25eefb62f3f95d5bef8
503 <503@academicos.cesde.edu.co>
2023-02-23 21:00:33 -0500

    ejemplos de reactNative

---
+++ assets/styles/styles1.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'green',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    borders:{
+      borderWidth:3,
+      borderColor:"black"
+    }
+  
+  });
+  
+  const mytexts = StyleSheet.create({
+    text:{
+      color:'red'
+    }
+  })
+
+  export{styles,mytexts}
+  
